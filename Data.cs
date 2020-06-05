55720dcf8cb066f5e5844b182b834c6a96bb98cf
Uncle <alex.the.kovalenko@gmail.com>
2020-06-05 22:15:08 +0300

    Added other notes. Now the keys 'Z' - '/' are C3 - E4 notes. And corresponding keys from the line above are the black keys.

--- Data.cs
+++ Data.cs
@@ -7,18 +7,23 @@ namespace SynthFromScratch
     {
         Dictionary<Keys, float> frequencies = new Dictionary<Keys, float>()
         {
-            { Keys.Z, 130f },   // C3
-            { Keys.S, 138f },   // C#3
-            { Keys.X, 146f },   // D3
-            { Keys.D, 155f },   // D#3
-            { Keys.C, 164f },   // E3
-            { Keys.V, 174f },   // F3
+            { Keys.Z, 130.8f },   // C3
+            { Keys.S, 138.6f },   // C#3
+            { Keys.X, 146.8f },   // D3
+            { Keys.D, 155.6f },   // D#3
+            { Keys.C, 164.8f },   // E3
+            { Keys.V, 174.6f },   // F3
             { Keys.G, 185f },   // F#3
             { Keys.B, 196f },   // G3
-            { Keys.H, 208f },   // G#3
+            { Keys.H, 208.7f },   // G#3
             { Keys.N, 220f },   // A3
-            { Keys.J, 233f },   // A#3
-            { Keys.M, 246f },   // B3
+            { Keys.J, 233.1f },   // A#3
+            { Keys.M, 246.9f },   // B3
+            { Keys.Oemcomma, 261.6f },  // C4
+            { Keys.L, 277.2f }, // C#4
+            { Keys.OemPeriod, 293.7f }, // D4
+            { Keys.Oem1, 311.1f },   // D#4
+            { Keys.OemQuestion, 329.6f },   // E4
         };
     }
 }
